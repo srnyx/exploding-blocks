6ac5a9558d8cb20bbed65fb08ea340bc7a720ea3
srnyx <25808801+srnyx@users.noreply.github.com>
2024-11-09 17:44:00 -0500

    Add support for 1.14.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,8 @@ plugins {
     id("com.gradleup.shadow") version "8.3.5"
 }
 
-spigotAPI("1.14.3")
 setupAnnoyingAPI("5.1.3", "xyz.srnyx", "2.0.1", "Every block you break will explode")
+spigotAPI("1.14.2")
 
 repository(Repository.PLACEHOLDER_API)
 dependencies.compileOnly("me.clip", "placeholderapi", "2.11.6")
